eb9d282b58069d2c39efec413960b10db042e4a5
Nitanshu12 <nitanshugoyal786@gmail.com>
2025-05-03 16:39:39 +0530

    first animation

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,14 @@
 const config = {
+  theme:{
+    extend:{
+      colors:{
+        black:{
+          DEFAULT:'#000319',
+          100:'#000319'
+        }
+      }
+    }
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
